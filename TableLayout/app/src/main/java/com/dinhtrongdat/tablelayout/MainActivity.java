22a9d3ae8735b23ca87d0c32e9e6fb11d277da1e
Đinh Trọng Đạt <71766267+Dat0309@users.noreply.github.com>
2021-07-25 21:48:56 +0700

    Table layout

---
+++ TableLayout/app/src/main/java/com/dinhtrongdat/tablelayout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dinhtrongdat.tablelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import  android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
