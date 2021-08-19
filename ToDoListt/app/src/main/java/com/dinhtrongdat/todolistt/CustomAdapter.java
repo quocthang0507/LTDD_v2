1f9f5bf4aa436ca2b6d8a900185d756edd8d2677
Đinh Trọng Đạt <71766267+Dat0309@users.noreply.github.com>
2021-08-19 09:52:01 +0700

    Uứng dụng To do list

--- ToDoListt/app/src/main/java/com/dinhtrongdat/todolistt/CustomAdapter.java
+++ ToDoListt/app/src/main/java/com/dinhtrongdat/todolistt/CustomAdapter.java
@@ -1,6 +1,8 @@
 package com.dinhtrongdat.todolistt;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,11 +18,11 @@ import androidx.annotation.Nullable;
 import java.util.List;
 
 public class CustomAdapter extends ArrayAdapter {
-    private Context context;
+    private MainActivity context;
     private int layout;
     private List<CongViec> list;
 
-    public CustomAdapter(@NonNull Context context, int resource, @NonNull List objects) {
+    public CustomAdapter(@NonNull MainActivity context, int resource, @NonNull List objects) {
         super(context, resource, objects);
         this.context = context;
         this.layout = resource;
@@ -36,6 +39,8 @@ public class CustomAdapter extends ArrayAdapter {
             convertView = LayoutInflater.from(context).inflate(R.layout.layout_adapter, parent, false);
 
             viewHolder.txtTen = (TextView) convertView.findViewById(R.id.txtName);
+            viewHolder.BtnEdit = (ImageButton) convertView.findViewById(R.id.btnEdit);
+            viewHolder.btnDelete = (ImageButton) convertView.findViewById(R.id.btnDelete);
 
             convertView.setTag(viewHolder);
         }
@@ -46,12 +51,28 @@ public class CustomAdapter extends ArrayAdapter {
         CongViec cv = list.get(position);
         viewHolder.txtTen.setText(cv.getTenCV());
 
+
+        viewHolder.BtnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context.DialogEdit(cv.getTenCV(), cv.getId());
+            }
+        });
+        viewHolder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context.DeleteQuery(cv.getTenCV(), cv.getId());
+            }
+        });
+
         return convertView;
     }
 
     private class ViewHolder{
         TextView txtTen;
         ImageButton BtnEdit, btnDelete;
+
     }
+
 
 }
--- ToDoListt/app/src/main/java/com/dinhtrongdat/todolistt/MainActivity.java
+++ ToDoListt/app/src/main/java/com/dinhtrongdat/todolistt/MainActivity.java
@@ -11,7 +11,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -25,14 +28,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        list = new ArrayList<>();
         setWidget();
+        list = new ArrayList<>();
+        adapter = new CustomAdapter(MainActivity.this, R.layout.layout_adapter,list);
 
         db = new Database(MainActivity.this, "congviec.sqlite",null, 1);
         db.QueryData("CREATE TABLE IF NOT EXISTS CongViec (Id INTEGER PRIMARY KEY AUTOINCREMENT, TenCV NVARCHAR(200))");
 
-        adapter = new CustomAdapter(MainActivity.this, R.layout.layout_adapter,list);
-        adapter.setNotifyOnChange(true);
+//        db.QueryData("INSERT INTO CongViec VALUES(null, 'Android self Study')");
+//        db.QueryData("DELETE FROM CongViec");
+
+        list.clear();
+        Cursor cursor = db.GetData("SELECT * FROM CongViec");
+        while (cursor.moveToNext()){
+            list.add(new CongViec(cursor.getInt(0),cursor.getString(1)));
+        }
+
+        adapter.notifyDataSetChanged();
         listView.setAdapter(adapter);
 
     }
@@ -66,21 +78,103 @@ public class MainActivity extends AppCompatActivity {
         Button btnDialog_add = (Button) dialog.findViewById(R.id.dialog_btn_add);
         Button btnDialog_edit = (Button) dialog.findViewById(R.id.dialog_btn_exit);
 
-        String name = edtDialog.getText().toString().trim();
 
         btnDialog_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                db.QueryData("INSERT INTO CongViec VALUES (null, '"+ name +"')");
-
-                Cursor cursor = db.GetData("SELECT * FROM CongViec");
-                while (cursor.moveToNext()){
-                    list.add(new CongViec(cursor.getInt(0),cursor.getString(1)));
+                if (edtDialog.getText().equals("")){
+                    Toast.makeText(MainActivity.this, "Chua dien ten cong viec", Toast.LENGTH_SHORT).show();
                 }
+                else {
+                    db.QueryData("INSERT INTO CongViec VALUES (null, '" + edtDialog.getText() + "')");
+
+                    list.clear();
+                    Cursor cursor = db.GetData("SELECT * FROM CongViec");
+                    while (cursor.moveToNext()){
+                        list.add(new CongViec(cursor.getInt(0),cursor.getString(1)));
+                    }
+
+                    adapter.notifyDataSetChanged();
+
+                    Toast.makeText(MainActivity.this, "Success!!", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+
             }
         });
 
         btnDialog_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+    public void DialogEdit(String ten,final int id){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_edit);
+
+        EditText edt_edit = (EditText) dialog.findViewById(R.id.dialog_edt_edit);
+        Button btnOk = (Button) dialog.findViewById(R.id.dialog_btn_ok);
+        Button btnClose = (Button) dialog.findViewById(R.id.dialog_btn_close);
+
+        edt_edit.setText(ten);
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.QueryData("UPDATE CongViec SET TenCV ='"+ edt_edit.getText()+"' WHERE Id = '"+id+"'");
+                list.clear();
+                Cursor cursor = db.GetData("SELECT * FROM CongViec");
+                while (cursor.moveToNext()){
+                    list.add(new CongViec(cursor.getInt(0),cursor.getString(1)));
+                }
+
+                adapter.notifyDataSetChanged();
+                dialog.dismiss();
+            }
+        });
+
+
+        dialog.show();
+    }
+
+    public void DeleteQuery(String ten, final int id){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_delete);
+
+        Button btn_OK = (Button) dialog.findViewById(R.id.dialog_delete_ok);
+        Button btn_Exit = (Button) dialog.findViewById(R.id.dialog_delete_exit);
+        TextView txtView = (TextView) dialog.findViewById(R.id.txt_delete);
+
+        txtView.setText("Bạn có muốn xóa công việc "+ ten+" không?");
+
+        btn_OK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                db.QueryData("DELETE FROM CongViec WHERE Id ='"+ id +"' ");
+
+                list.clear();
+                Cursor cursor = db.GetData("SELECT * FROM CongViec");
+                while (cursor.moveToNext()){
+                    list.add(new CongViec(cursor.getInt(0),cursor.getString(1)));
+                }
+
+                adapter.notifyDataSetChanged();
+                dialog.dismiss();
+            }
+        });
+        btn_Exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dialog.dismiss();
