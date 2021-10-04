6aece7730cdd577e10e249536a3f65ae8d4bb236
Đinh Trọng Đạt <71766267+Dat0309@users.noreply.github.com>
2021-10-04 07:35:58 +0700

    ứng dụng truyện cười

---
+++ Lab10-11/TruyenCuoi/Truyencuoi/app/src/main/java/com/dinhtrongdat/truyencuoi/MainActivity.java
@@ -0,0 +1,14 @@
+package com.dinhtrongdat.truyencuoi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
