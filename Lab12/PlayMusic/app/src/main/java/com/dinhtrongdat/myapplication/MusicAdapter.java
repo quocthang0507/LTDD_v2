bf6db120716e3fae75657257a026b8acaf0ce6f6
Đinh Trọng Đạt <71766267+Dat0309@users.noreply.github.com>
2021-10-15 16:58:02 +0700

    lab12

---
+++ Lab12/PlayMusic/app/src/main/java/com/dinhtrongdat/myapplication/MusicAdapter.java
@@ -0,0 +1,50 @@
+package com.dinhtrongdat.myapplication;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MusicHolder> {
+    private final ArrayList<SongEntity> listSong;
+    private final Context mContext;
+
+    public MusicAdapter(ArrayList<SongEntity> listSong, Context mContext) {
+        this.listSong = listSong;
+        this.mContext = mContext;
+    }
+
+    @Override
+    public MusicHolder onCreateViewHolder( ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_song, parent, false);
+        return new MusicHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MusicAdapter.MusicHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listSong.size();
+    }
+
+    public class MusicHolder extends RecyclerView.ViewHolder {
+        TextView tvName;
+        public MusicHolder(View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tv_song);
+            itemView.setOnClickListener(v -> {
+                v.startAnimation(AnimationUtils.loadAnimation(mContext, androidx.appcompat.R.anim.abc_fade_in));
+                ((MainActivity) mContext).playSong((SongEntity)tvName.getTag());
+            });
+        }
+    }
+}
