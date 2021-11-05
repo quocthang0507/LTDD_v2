634e15dfe056f85bf3ff6111fda6cac0e509161d
Đinh Trọng Đạt <71766267+Dat0309@users.noreply.github.com>
2021-11-05 17:01:34 +0700

    Update lab 14-15

---
+++ Lab14-15/lab14-15/app/src/main/java/com/example/lab14_15/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.lab14_15;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lab14_15.fragment.M000LoginFragment;
+import com.example.lab14_15.fragment.M000RegisterFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String SAVE_PREF = "save_pref";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gotoLoginScreen();
+
+    }
+
+    public void gotoRegisterScreen() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.ln_main, new M000RegisterFragment()).commit();
+    }
+
+    public void gotoLoginScreen() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.ln_main, new M000LoginFragment()).commit();
+    }
+}
